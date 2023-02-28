80e3566675c615778e867ac0f018403556f87a2a
Kanel04 <thelegendanelka@gmail.com>
2023-02-28 09:20:24 +0300

    responsive

--- src/pages/Home/components/header/index.jsx
+++ src/pages/Home/components/header/index.jsx
@@ -27,18 +27,18 @@ function Header() {
   };
 
   return (
-    <div id='about' className='flex justify-center h-screen bg-[#ede9fe] py-8 '>
+    <div id='about' className='flex justify-center lg:h-screen bg-[#ede9fe] lg:py-8 py-5'>
       
-      <div className='flex flex-row  bg-white shadow-xl rounded-xl  ml-20 mt-20 mr-20 '>
-        <div className='w-96 h-full  overflow-hidden bg-no-repeat bg-cover hover:rounded-l-xl '>
-        <img src={Anelka} alt="AnelkaPhoto" className='w-96 h-full rounded-l-xl  hover:scale-110 transition duration-300 ease-in-out hover:rounded-l-xl ' />
+      <div className='flex lg:flex-row flex-col bg-white shadow-xl rounded-xl  lg:ml-20 lg:mt-20 lg:mr-20  '>
+        <div className='lg:w-96 lg:h-full  overflow-hidden bg-no-repeat bg-cover hover:rounded-l-xl  '>
+        <img src={Anelka} alt="AnelkaPhoto" className='lg:w-96 lg:h-full h-80 w-80 lg:rounded-l-xl rounded-t-xl  hover:scale-110 transition duration-300 ease-in-out hover:rounded-l-xl ' />
         </div>
        
-        <div className='w-96 h-full  m-5'>
-        <p className='font-bold text-5xl text-[#3f3f46] '>JAOSOA Donga Anelka</p>
-          <p className='text-2xl font-bold mt-8 text-[#52525b] '>Developer Fullstack , passionate of programming langage, designers and 3D developer. I can work with group </p>
-          <p className='text-xl font-bold mt-5 text-[#52525b]'>Member of google developers groups GDG Mahajanga</p>
-          <div className='flex flex-row mt-5'>
+        <div className='lg:w-96 lg:h-full w-40 h-96  m-8'>
+        <p className='font-bold lg:text-5xl text-2xl text-[#3f3f46] '>JAOSOA Donga Anelka</p>
+          <p className='lg:text-2xl text-sm font-bold lg:mt-8 mt-4 text-[#52525b] '>Developer Fullstack , passionate of programming langage, designers and 3D developer. I can work with group </p>
+          <p className='lg:text-xl text-sm font-bold mt-5 text-[#52525b]'>Member of google developers groups GDG Mahajanga</p>
+          <div className='flex flex-row lg:mt-5 mt-2'>
 <MdPlace className='text-xl text-[#5b21b6] hover:underline'/>         
  <a href='https://www.google.com/maps/place/Ambohimandamina/@-15.7165647,46.3427353,17z/data=!3m1!4b1!4m6!3m5!1s0x2203fbc7d5078047:0xd2cab551c012a77b!8m2!3d-15.7165647!4d46.344924!16s%2Fg%2F11jsghnl3k'
  className='ml-1 text-[#5b21b6] hover:underline'
@@ -46,7 +46,7 @@ function Header() {
   Ambohimandamina , Mahajanga</a>
 </div>
 <div>
-  <button className='text-white font-bold bg-[#5b21b6] mt-14 py-3 px-7 rounded-xl ml-4 '
+  <button className='text-white font-bold bg-[#5b21b6] lg:mt-14 mt-2 lg:py-3 lg:px-7 px-4 lg:text-xl text-sm lg:rounded-xl py-3 rounded-lg  ml-4 '
   onClick={handleDownload} disabled={loading}
   >
     {loading ? "Downloading..." : "Download CV"}
--- src/pages/Home/components/navbar/index.jsx
+++ src/pages/Home/components/navbar/index.jsx
@@ -5,52 +5,57 @@ import Anelka from '../assets/anelka-r.png';
 import {FaBars} from 'react-icons/fa';
 import {motion} from 'framer-motion'
 function Navbar() {
-
+  function Menu(e){
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+  }
   return (
     <div>
          <div>
-            <div id='home' className='flex flex-col    w-full h-screen  '>
-              <div className="flex flex-row w-full h-16 shadow-lg fixed  bg-white  ">              
+            <div id='home' className='flex flex-col    lg:w-full   lg:h-screen  '>
+              <div className="flex flex-row lg:w-full lg:h-16 w-screen shadow-lg fixed  bg-white h-16 ">              
                  <div className='p-5'>
-                <a href="#home" className='flex flex-row text-3xl  font-bold text-[#3f3f46] '><p className='font-bold text-[#5b21b6] text-3xl '>P</p>ortfolio.</a>
+                <a href="#home" className='flex flex-row lg:text-3xl text-xl font-bold text-[#3f3f46] '><p className='font-bold text-[#5b21b6] lg:text-3xl text-xl '>P</p>ortfolio.</a>
                 </div>
-                <div className='flex justify-center ml-60 '>
+                <div className='flex lg:flex-row flex-col lg:justify-center lg:ml-60 flex-col '>
                 <div className='p-5'>
-<a  href="#home" className=' font-bold text-xl text-[#3f3f46] '  >Home</a>
+<a  href="#home" className=' font-bold lg:text-xl text-[#3f3f46] '  >Home</a>
                 </div>
                 <div className='p-5'>
-<a href="#contact" className=' font-bold text-xl text-[#3f3f46] '>Contact</a>
+<a href="#contact" className=' font-bold lg:text-xl text-[#3f3f46] '>Contact</a>
                 </div>
                 <div className='p-5 '>
-<a href="#about" className=' font-bold text-xl text-[#3f3f46]'>About</a>
+<a href="#about" className=' font-bold lg:text-xl text-[#3f3f46]'>About</a>
                 </div>
                 <div className='p-5'>
-<a href="#skills" className=' font-bold text-xl text-[#3f3f46]'>Skills</a>
+<a href="#skills" className=' font-bold lg:text-xl text-[#3f3f46]'>Skills</a>
                 </div>
                 <div className='p-5'>
-<a href="#services" className=' font-bold text-xl text-[#3f3f46]'>Services</a>
+<a href="#services" className=' font-bold lg:text-xl text-[#3f3f46]'>Services</a>
                 </div>
-                <div className='pl-5 py-5 flex flex-row ml-40'>
-                  <AiFillLinkedin className=' text-2xl text-[#5b21b6]' />
-<a href="https://www.linkedin.com/in/anelka-donga-289b8122a/" className=' font-bold text-xl ml-2 cursor-pointer text-[#3f3f46]'>LinkdIn</a>
+                <div className='pl-5 py-5 flex flex-row lg;ml-40 xl:ml-40  '>
+                  <AiFillLinkedin className=' lg:text-2xl text-xl text-[#5b21b6]' />
+<a href="https://www.linkedin.com/in/anelka-donga-289b8122a/" className=' font-bold lg:text-xl ml-2 cursor-pointer text-[#3f3f46]'>LinkdIn</a>
+                </div>
+                <div className='pl-5 py-5 flex flex-row'>
+                <BsGithub className=' lg:text-2xl text-xl text-[#5b21b6]'/>
+<a href="https://github.com/Kanel04" className=' font-bold lg:text-xl ml-2 cursor-pointer text-[#3f3f46]'> GitHuB</a>
                 </div>
                 </div>
         
-                <div className='pl-5 py-5 flex flex-row'>
-                <BsGithub className=' text-2xl text-[#5b21b6]'/>
-<a href="https://github.com/Kanel04" className=' font-bold text-xl ml-2 cursor-pointer text-[#3f3f46]'> GitHuB</a>
+             
+                <FaBars  className=' text-2xl mt-5  lg:hidden ml-40 text-[#3f3f46] ' >  </FaBars>
                 </div>
-                </div>
-            <div className='flex flex-row'>
+            <div className='flex lg:flex-row flex-col justify-center m-5'>
             <div 
      
-            className='bg-[#5b21b6] w-96 h-96 mt-24 ml-40 rounded-full shadow-xl '>
-                  <p className='text-9xl font-bold  text-white text-center mt-24 ml-14 flex flex-row  '>I'm <p className='text-white font-bold text-5xl mt-20 ml-24 '>a</p> </p>
+            className='bg-[#5b21b6] lg:w-96 lg:h-96 w-60 h-60  lg:mt-24 mt-32 ml-40 mt-24 rounded-full shadow-xl '>
+                  <p className='lg:text-9xl text-4xl font-bold  text-white text-center lg:mt-24 mt-24  ml-14 flex flex-row  '>I'm <p className='text-white font-bold lg:text-5xl lg:mt-20 lg:ml-24 mt-1 ml-2 text-3xl '>a</p> </p>
             </div>
-            <div className='mt-40 '>
-              <p className='flex flex-row font-bold text-5xl bg-black w-80 h-80 rounded-full py-24 text-white mt-5'><p className='text-[#5b21b6] font-bold text-5xl ml-6'>F</p>ullstack <p className='text-[#5b21b6] ml-12 font-bold text-5xl flex flex-row mt-8'><p className='text-white font-bold text-5xl ml-1'>D</p>eveloper</p></p>
+            <div className='lg:mt-40  '>
+              <p className='flex flex-row font-bold lg:text-5xl text-4xl bg-black lg:w-80 lg:h-80 w-60 h-60  rounded-full py-24 text-white mt-5'><p className='text-[#5b21b6] font-bold lg:text-5xl text-4xl ml-6'>F</p>ullstack <p className='text-[#5b21b6] lg:ml-12 ml-6 font-bold lg:text-5xl text-4xl flex flex-row mt-8'><p className='text-white font-bold lg:text-5xl text-4xl ml-1'>D</p>eveloper</p></p>
             </div>
-            <div className="ml-24">
+            <div className="lg:ml-24">
               <img src={Anelka} alt="photo" />
             </div>
             </div>
